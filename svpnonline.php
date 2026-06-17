9223882db1e801df0fe8d0046c93bf9bb19824e6
sansoe2022 <106647481+sansoe2022@users.noreply.github.com>
2026-06-18 06:10:46 +0700

    Add US 1 domain to allowed domains and server list

--- svpnonline.php
+++ svpnonline.php
@@ -5,7 +5,7 @@ if (isset($_GET['fetch_url'])) {
     
     // လုံခြုံရေးအတွက်၊ ခွင့်ပြုထားတဲ့ domain တွေကိုပဲ request လုပ်ခိုင်းပါမယ်။
     $allowed_domains = [
-        
+        'us1.sksvpn.shop',
         'svpnjp1.sksvpn.shop',
         'svpnjp2.sksvpn.shop',
         'svpnfree1may.sksvpn.shop',
@@ -343,6 +343,10 @@ header("Content-Security-Policy: frame-ancestors *;");
     <script>
         // Server list (သင့် server list အတိုင်း)
         const servers = {
+            '🇺🇲 US 1': [
+                'http://us1.sksvpn.shop:81/server/online',
+                'http://us1.sksvpn.shop:81/udpserver/online'
+            ],
             '🇯🇵 JP 1': [
                 'http://svpnjp1.sksvpn.shop:81/server/online',
                 'http://svpnjp1.sksvpn.shop:81/udpserver/online'
